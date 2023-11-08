2ec74aa51cc8a40809292674e9aba497a0d1d3e5
Luigi-Iorio <luigi0iorio@gmail.com>
2023-11-08 16:09:57 +0100

    feat: elenco numeri e testi in console

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 "use strict";
+// dichiarazione / assegnazione varibilli
+const fizz = "Fizz";
+const buzz = "Buzz";
+const fizzBuzz = "FizzBuzz";
+
+// visualizzazione in console dell'elenco (numeri/testo)
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log(fizzBuzz);
+  } else if (i % 3 === 0) {
+    console.log(fizz);
+  } else if (i % 5 === 0) {
+    console.log(buzz);
+  } else {
+    console.log(i);
+  }
+}
